525bbfc967576bb1b5783a3b9268950a28c34daa
dev <vverja@gmail.com>
2020-08-25 23:52:31 +0300

    users-list hw1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'users-list';
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
